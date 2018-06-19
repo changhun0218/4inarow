aa015c780f9454bdd1361772a0371d069760f3af
developmentofdevelopment <v55ohv@gmail.com>
2018-06-19 18:19:37 +0900

    MCTS file is created

---
+++ mcts.py
@@ -0,0 +1,26 @@
+"""
+Monte Carlo Tree Search (MCTS)
+"""
+
+"""
+MCTS will be executed for a single move and store the information of the move.
+The information of moves will be used for neural network to learn
+MCTS also will be guided by the neural network.
+"""
+
+class MCTS:
+    def __init__(self):
+        """
+        N(s, a) : a visit count
+        W(s, a) : a total action value
+        P(s, a) : a prior probability
+        Q(s, a) : a mean action value
+        """
+    def select(self):
+
+    def expand(self):
+
+    def eval(self):
+        
+    def backup(self):
+        
